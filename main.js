1532c96591dde120aefab05993c9c3ed2d895a9a
rossmclenny <rossmclennan85@gmail.com>
2020-11-06 17:02:30 +0000

    loop correct results logging ok

--- main.js
+++ main.js
@@ -1,12 +1,49 @@
-function check() {
+function check () {
     var c = 0
+    var w = 0
     var radios = document.getElementsByName('question1');
+    var radios2 = document.getElementsByName('question2');
+    var radios3 = document.getElementsByName('question3');
+// question 1
     for (var i = 0; i < radios.length; i++) {
-    if (radios[i].checked == "1") {
-        c++;
-        console.log(c);
-        break;
-    }
-    }
-}
-console.log(c);
+    if (radios[i].checked) 
+    {
+        if (radios[i].value == "Lars") 
+            {
+            c++
+            } else 
+            {
+            w++
+            } 
+        } 
+    } 
+// question 2
+    for (var i = 0; i < radios2.length; i++) {
+    if (radios2[i].checked) 
+    {
+        if (radios2[i].value == "Tom") 
+            {
+            c++
+            } else
+            {
+             w++    
+            } 
+        } 
+    } 
+// question 3
+    for (var i = 0; i < radios3.length; i++) {
+    if (radios3[i].checked) 
+    {
+        if (radios3[i].value == "David") 
+            {
+            c++
+            } else
+            {
+            w++   
+            } 
+        }
+    } 
+    console.log(c);
+    console.log(w);
+} //end of check function
+
